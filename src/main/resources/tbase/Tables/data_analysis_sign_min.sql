a3e33dc6eda8f0df09a3689775608269ad9eabd3
wangbingkai <wangbingkai@e-sscard.com>
2020-07-07 10:53:53 +0800

    idx.sql

--- src/main/resources/tbase/Tables/data_analysis_sign_min.sql
+++ src/main/resources/tbase/Tables/data_analysis_sign_min.sql
@@ -1,6 +1,6 @@
 --- 每分钟签发量
-drop table if exists data_analysis_sign_min;
-create table data_analysis_sign_min(
+drop table if exists public.data_analysis_sign_min
+create table public.data_analysis_sign_min(
 id bigserial not null primary key,
 collect_time timestamp without time zone,
 channel_no varchar(32),
@@ -10,11 +10,14 @@ create_time timestamp without time zone default now(),
 status integer default 1
 )
 distribute by shard(id) to group default_group;
-comment on table data_analysis_sign_min is '签发量统计表';
-comment on column data_analysis_sign_min.id is '自增主键';
-comment on column data_analysis_sign_min.collect_time is '统计时间';
-comment on column data_analysis_sign_min.channel_no is '渠道号';
-comment on column data_analysis_sign_min.card_region_code is '签发区域划分';
-comment on column data_analysis_sign_min.transfer_times is '签发数量';
-comment on column data_analysis_sign_min.create_time is '创建时间';
-comment on column data_analysis_sign_min.status is '数据状态';
+comment on table public.data_analysis_sign_min is '签发量统计表';
+comment on column public.data_analysis_sign_min.id is '自增主键';
+comment on column public.data_analysis_sign_min.collect_time is '统计时间';
+comment on column public.data_analysis_sign_min.channel_no is '渠道号';
+comment on column public.data_analysis_sign_min.card_region_code is '签发区域划分';
+comment on column public.data_analysis_sign_min.transfer_times is '签发数量';
+comment on column public.data_analysis_sign_min.create_time is '创建时间';
+comment on column public.data_analysis_sign_min.status is '数据状态';
+
+create index idx_collect_time on public.data_analysis_sign_min (collect_time);
+
